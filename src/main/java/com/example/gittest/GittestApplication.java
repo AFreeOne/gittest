27ea152072370942dc242bdbf599a0f2d828a32d
TheFreeOne <1357441309@qq.com>
2020-08-17 14:10:53 +0800

    01

--- src/main/java/com/example/gittest/GittestApplication.java
+++ src/main/java/com/example/gittest/GittestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GittestApplication {
-
+    // 阿斯顿阿三
     public static void main(String[] args) {
         SpringApplication.run(GittestApplication.class, args);
     }
